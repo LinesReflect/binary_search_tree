72a504de0b13bc8704b02a30b87f308dfd0a5095
Reflection <reflectionoflines@gmail.com>
2023-02-28 11:01:49 -0600

    Set the default values of initialized attributes to nil.

--- lib/node.rb
+++ lib/node.rb
@@ -1,7 +1,7 @@
 class Node
   attr_accessor :data, :left_child, :right_child
 
-  def initialize(data, left_child, right_child)
+  def initialize(data = nil, left_child = nil, right_child = nil)
     @data = data
     @left_child = left_child
     @right_child = right_child
