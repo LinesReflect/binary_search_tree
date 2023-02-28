14437d3ba5484ab67031e1dc8e5ca0843d31832a
Reflection <reflectionoflines@gmail.com>
2023-02-28 11:00:46 -0600

    Refactor initialize method and create build tree method.

--- lib/tree.rb
+++ lib/tree.rb
@@ -1,5 +1,11 @@
 class Tree
+  attr_accessor :array, :root
+
   def initialize(array)
-    @array = array
+    @array = array.uniq.sort
+    @root = build_tree(@array)
+  end
+
+  def build_tree(data)  
   end
 end
