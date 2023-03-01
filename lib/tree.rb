fc0532d02bae6f4d4ceb589f1ae404e07cb40a6f
Reflection <reflectionoflines@gmail.com>
2023-03-01 10:44:48 -0600

    Finish the build tree method.

--- lib/tree.rb
+++ lib/tree.rb
@@ -6,6 +6,17 @@ class Tree
     @root = build_tree(@array)
   end
 
-  def build_tree(data)  
+  def build_tree(data, first = 0, last = data.length - 1, middle = (first + last) / 2)
+    return Node.new(data) if data.length == 1
+
+    if data.length == 2
+      left = [data[first]]
+      right = [data[last]]
+    else
+      left = data[first..(middle - 1)]
+      right = data[(middle + 1)..last]
+    end
+
+    Node.new(data[middle], build_tree(left), build_tree(right))
   end
 end
