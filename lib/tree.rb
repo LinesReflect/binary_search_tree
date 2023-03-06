fe273c63477793a65f9d5712ba3c6499c8d86fab
Reflection <reflectionoflines@gmail.com>
2023-03-06 12:09:38 -0600

    Create the delete method and leaf_node method.

--- lib/tree.rb
+++ lib/tree.rb
@@ -37,4 +37,35 @@ class Tree
       current_node.right_child.nil? ? current_node.right_child = Node.new(value) : insert(value, current_node.right_child)
     end
   end
+
+
+  def delete(value, current_node = @root, parent_node = nil)
+    if value < current_node.data
+      delete(value, current_node.left_child, current_node) 
+    elsif current_node.data < value
+      delete(value, current_node.right_child, current_node)
+    else
+      return parent_node.left_child == current_node ? parent_node.left_child = nil : parent_node.right_child = nil if leaf_node?(current_node)
+      return parent_node.left_child = current_node.right_child if current_node.left_child.nil?
+      return parent_node.right_child = current_node.left_child if current_node.right_child.nil?
+
+      replacement_node = current_node.right_child
+      replacement_node_parent = replacement_node
+      until replacement_node.left_child.nil?
+        replacement_node_parent = replacement_node
+        replacement_node = replacement_node.left_child
+      end
+      delete(replacement_node.data)
+      replacement_node.left_child = current_node.left_child
+      replacement_node.right_child = current_node.right_child
+      if parent_node.nil?
+        return @root = replacement_node
+      end
+      parent_node.left_child == current_node ? parent_node.left_child = replacement_node : parent_node.right_child = replacement_node
+    end
+  end
+
+  def leaf_node?(node)
+    node.left_child.nil? && node.right_child.nil?
+  end
 end
